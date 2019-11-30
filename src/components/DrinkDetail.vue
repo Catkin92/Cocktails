a4c89ec2677f8bad9d54152386c4e5943ad8815d
Catrin <catrinhughes92@gmail.com>
2019-11-30 19:50:51 +0000

    mocktail detail made

--- src/components/DrinkDetail.vue
+++ src/components/DrinkDetail.vue
@@ -1,7 +1,19 @@
 <template>
-  <div v-if="drink">
-    <h2>{{drink.drinks[0].strDrink}}</h2>
-    <p>{{drink.drinks[0].strInstructions}}</p>
+  <div v-if="drink" id="info">
+    <label v-for="detail in drink.drinks">
+    <h2>{{detail.strDrink}}</h2>
+    <ul>
+      <li v-if="detail.strIngredient1">{{detail.strMeasure1}} {{detail.strIngredient1}}</li>
+      <li v-if="detail.strIngredient2">{{detail.strMeasure2}} {{detail.strIngredient2}}</li>
+      <li v-if="detail.strIngredient3">{{detail.strMeasure3}} {{detail.strIngredient3}}</li>
+      <li v-if="detail.strIngredient4">{{detail.strMeasure4}} {{detail.strIngredient4}}</li>
+      <li v-if="detail.strIngredient5">{{detail.strMeasure5}} {{detail.strIngredient5}}</li>
+      <li v-if="detail.strIngredient6">{{detail.strMeasure6}} {{detail.strIngredient6}}</li>
+      <li v-if="detail.strIngredient7">{{detail.strMeasure7}} {{detail.strIngredient7}}</li>
+
+    </ul>
+    <p><i>{{detail.strInstructions}}</i></p>
+    </label>
   </div>
 </template>
 
@@ -13,4 +25,11 @@ export default {
 </script>
 
 <style lang="css" scoped>
+  #info {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  }
 </style>
